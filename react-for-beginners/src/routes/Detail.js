55b3ddce32a0eb6dfbba1c24809f9069d69b5bdf
ICubE- <jehoyeon01@gmail.com>
2024-02-06 23:10:59 +0900

    Update

--- react-for-beginners/src/routes/Detail.js
+++ react-for-beginners/src/routes/Detail.js
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Movie from "../components/Movie.js"
+
 function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState({});
+  const {id} = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+  }
+  useEffect(() => {
+    getMovie();
+  }, []);
+  console.log(movie);
   return (
-    <h1>Detail</h1>
+    <div>
+      <h1>Detail</h1>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <Movie 
+          key={id}
+          id={Number.parseInt(id)}
+          coverImg={movie.large_cover_image}
+          title={movie.title}
+          summary={movie.description_full}
+          genres={movie.genres}
+        />
+      )}
+    </div>
   );
 }
 
